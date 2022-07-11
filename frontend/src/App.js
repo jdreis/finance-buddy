4fd0056bc9b534538b7efe30e85cf22d63cd7f8c
Jena Reis <jenadreis@gmail.com>
2022-07-11 11:33:43 -0400

    set up file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route,  } from "react-router-dom";
+
+import "./App.css";
+
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <header>
-        Welcome to Finance Buddy!
-      </header>
+      <NavBar />
+
+      
+        <Route path="/" component={NavBar} />
+      
     </div>
+  </Router>
   );
 }
 
